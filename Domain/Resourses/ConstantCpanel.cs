e42897b52fefb2ea759296500e478fdd139b9480
Raesgithub <productraes@gmail.com>
2024-11-05 18:02:06 +0330

    sss

---
+++ Domain/Resourses/ConstantCpanel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Resourses
+{
+    public static class ConstantCpanel
+    {
+        public  const string image_user_no_avatar = "/Cpanel/assets/media/image/user.png";
+    }
+}
