79a8189215dda7c73fd90291677a7facb1853287
Raesgithub <productraes@gmail.com>
2024-12-07 20:00:53 +0330

    dddd

--- Domain/ViewModels/UserVM.cs
+++ Domain/ViewModels/UserVM.cs
@@ -12,6 +12,8 @@ namespace Domain.ViewModels
         public string UserId { get; set; }
         public string Id { get; set; }
         public string? FullName { get; set; }
+        public string? Firstname { get; set; }
+        public string? Lastname { get; set; }
         public string? Email { get; set; }
         public string UserName { get; set; }
         public string? PhoneNumber { get; set; }
