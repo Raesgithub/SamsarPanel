c24e583dac6d144676ee3ee52d4bd2e5d62c9bd8
Raesgithub <productraes@gmail.com>
2024-12-29 16:34:37 +0330

    aaaa

---
+++ Application/Repositories/Shop/CatalogRepository.cs
@@ -0,0 +1,44 @@
+﻿using Application.Dtos.cpanel;
+using Dapper;
+using Domain.Models.shop;
+using Domain.Resourses;
+using Microsoft.Data.SqlClient;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Repositories.Shop
+{
+    public class CatalogRepository
+    {
+        public async Task<ResultDto> AddAsync(Catalog cat) 
+        {
+            try
+            {
+                var query = $@"insert into Catalogs(Name,Logo) values('{cat.Name}','{cat.Logo}')";
+                var res = 0;
+                using (var con = new SqlConnection(ConstantCpanel.connectionString))
+                {
+                    //isert update delete
+                    res = await con.ExecuteAsync(query);
+                }
+                if (res == 0)
+                {
+                    return new ResultDto { IsSuccess = false, Message = "قادر به افزودن دسته جدید نشدیم" };
+                }
+                else
+                {
+                    return new ResultDto { IsSuccess = true };
+                }
+            }
+            catch (Exception ex)
+            {
+
+                return new ResultDto { IsSuccess = false, Message = ex.InnerException != null ? ex.InnerException.Message : ex.Message };
+
+            }
+        }
+    }
+}
--- Domain/Data/ApplicationDbContext.cs
+++ Domain/Data/ApplicationDbContext.cs
@@ -1,3 +1,4 @@
+using Domain.Models.shop;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
@@ -6,5 +7,6 @@ namespace Domain.Data
 {
     public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : IdentityDbContext<ApplicationUser,IdentityRole,string>(options)
     {
+        public DbSet<Catalog> Catalogs { get; set; }
     }
 }
