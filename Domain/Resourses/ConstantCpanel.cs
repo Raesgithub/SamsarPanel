043753231a2b12c2febf66ba55d067db89cbe178
Raesgithub <productraes@gmail.com>
2025-09-17 16:03:09 +0330

    dddd

--- Domain/Resourses/ConstantCpanel.cs
+++ Domain/Resourses/ConstantCpanel.cs
@@ -9,10 +9,12 @@ namespace Domain.Resourses
     public static class ConstantCpanel
     {
         public const string image_user_Path = "Cpanel/assets/images/users";
+        public const string image_slider_Path = "Cpanel/assets/images/sliders";
         public const string image_product_Path = "Cpanel/assets/images/products";
 
         public static List<string> image_Valid_Types = new List<string> { ".jpg", ".png",".webp",".jpeg",".gif" };
         public static string pysicaly_image_user_path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", image_user_Path);
+        public static string pysicaly_image_slider_path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", image_slider_Path);
         public static string pysicaly_image_product_path = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", image_product_Path);
 
         public  const string image_user_no_avatar = "/Cpanel/assets/media/image/user.png";
