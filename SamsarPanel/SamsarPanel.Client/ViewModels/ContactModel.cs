679e0bfcb871811345bb9e99238934f8abb8e6f4
Samiyare <sameghbal0@gmail.com>
2025-10-04 16:42:14 +0330

    aaaaa

---
+++ SamsarPanel/SamsarPanel.Client/ViewModels/ContactModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SamsarPanel.Client.ViewModels
+{
+    public class ContactModel
+    {
+        [Display(Name = "نام کامل")]
+        public string FullName { get; set; }
+
+        [EmailAddress(ErrorMessage = "ایمیل معتبر نیست")]
+        [Display(Name = "ایمیل")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "شماره تلفن الزامی است")]
+        [RegularExpression(@"^(09(1[0-9]|2[0-9]|3[0-9]|9[0-9]))\d{7}$", ErrorMessage = "شماره معتبر وارد کنید (مثلاً 0912xxxxxxx)")]
+        [Display(Name = "شماره تلفن")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "موضوع الزامی است")]
+        [StringLength(90, MinimumLength = 5, ErrorMessage = "موضوع باید بین ۳ تا ۱۸ کلمه باشد")]
+        [Display(Name = "موضوع")]
+        public string Subject { get; set; }
+
+        [Required(ErrorMessage = "متن پیام الزامی است")]
+        [StringLength(512, MinimumLength = 32, ErrorMessage = "متن باید بین ۳۲ تا ۵۱۲ کاراکتر باشد")]
+        [Display(Name = "متن پیام")]
+        public string Message { get; set; }
+    }
+}
