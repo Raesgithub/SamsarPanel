bfb32183894fbd0a4be91fef70f289721eae89f7
Samiyare <sameghbal0@gmail.com>
2025-10-30 16:42:53 +0330

    sssssssss

---
+++ Domain/Migrations/20251030131240_n22222222.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Domain.Migrations
+{
+    /// <inheritdoc />
+    public partial class n22222222 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "IdTel",
+                table: "AspNetUsers",
+                newName: "UserNameTelegram");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "UserNameTelegram",
+                table: "AspNetUsers",
+                newName: "IdTel");
+        }
+    }
+}
